3dbd302dd3868ab6f4fcd89a2bc7a1948a6d53ef
elB4RTO <81904638+elB4RTO@users.noreply.github.com>
2022-06-09 21:08:25 +0200

    Update check.py

--- craplog/crappy/check.py
+++ craplog/crappy/check.py
@@ -104,9 +104,10 @@ def conflictFolder(
     """
     Ask the user what to do with the conflict
     """
+    craplog.printJobHalted()
     if craplog.more_output is True:
         print("\n")
-    else:
+    elif craplog.less_output is False:
         print()
     print("{warn}Warning{white}[{grey}conflict{white}]{warn}>{default} a folder is in conflict with the process: {grass}%s/{yellow}%s{default}"\
         .format(**craplog.text_colors)\
@@ -129,9 +130,10 @@ def conflictFile(
     """
     Ask the user what to do with the conflict
     """
+    craplog.printJobHalted()
     if craplog.more_output is True:
         print("\n")
-    else:
+    elif craplog.less_output is False:
         print()
     print("{warn}Warning{white}[{grey}conflict{white}]{warn}>{default} a file is in conflict with the process: {grass}%s/{yellow}%s{default}"\
         .format(**craplog.text_colors)\
@@ -154,19 +156,16 @@ def checkFolder(
     r: bool=False,
     w: bool=False,
     create:  bool=False,
-    resolve: bool=False,
-    undo:    bool=False
+    resolve: bool=False
 ) -> bool :
     """
     Check a crapstats folder
     """
     def failed():
-        nonlocal checks_passed, undo, craplog
+        nonlocal checks_passed, craplog
         if checks_passed is True:
             checks_passed = False
             craplog.printJobFailed()
-            if undo is True:
-                craplog.undoChanges()
     def makeit():
         nonlocal err_key, path, parent_path, entry_name, spaces, craplog
         # make the directory
@@ -316,19 +315,17 @@ def checkFile(
     r: bool=False,
     w: bool=False,
     create:  bool=False,
-    resolve: bool=False,
-    undo:    bool=False
+    resolve: bool=False
 ) -> bool :
     """
     Check a crapstats file
     """
     def failed():
-        nonlocal checks_passed, undo, craplog
+        nonlocal checks_passed, craplog
         if checks_passed is True:
             checks_passed = False
             craplog.printJobFailed()
-            if undo is True:
-                craplog.undoChanges()
+    
     if entry_name == ""\
     or parent_path == "":
         parent_path = path[:path.rfind('/')]
@@ -440,37 +437,49 @@ def makeInitialChecks( craplog ):
       - check internal variables for correctness/conflicts
       - check files/folders for existence and ability to read/write
     """
+    def failed():
+        nonlocal checks_passed, craplog
+        if checks_passed is True:
+            checks_passed = False
+            craplog.printJobFailed()
+    
     checks_passed = True
 
     # variables' integrity
     if  craplog.session_stats is False\
     and craplog.global_stats is False:
-        checks_passed = False
+        failed()
         print("\n{err}Error{white}[{grey}options_conflict{white}]{red}>{default} you can't use {cyan}--only-globals{default} toghether with {cyan}--avoid-globals{default}\n"\
             .format(**craplog.text_colors))
     if  craplog.access_logs is False\
     and craplog.error_logs is False:
-        checks_passed = False
+        failed()
         print("\n{err}Error{white}[{grey}variables_conflict{white}]{red}>{default} you can't avoid using both access and error log files, nothing will be done\n"\
             .format(**craplog.text_colors))
     if  craplog.access_logs is True\
     and len(craplog.access_fields) == 0:
-        checks_passed = False
+        failed()
         print("\n{err}Error{white}[{grey}missing_argument{white}]{red}>{default} you must use at least one field when working on access_logs\n"\
             .format(**craplog.text_colors))
     if  craplog.archive_tar is True\
     and craplog.archive_zip is True:
-        checks_passed = False
+        failed()
         print("\n{err}Error{white}[{grey}options_conflict{white}]{red}>{default} you can't use {cyan}--archive-tar{default} toghether with {cyan}--archive-zip{default}\n"\
+            .format(**craplog.text_colors))
+    if craplog.backup is False\
+    and (craplog.archive_tar is True\
+      or craplog.archive_zip is True):
+        failed()
+        print("\n{err}Error{white}[{grey}options_conflict{white}]{red}>{default} you can't archive a backup if you don't make one\n"\
             .format(**craplog.text_colors))
     if  craplog.trash is True\
     and craplog.shred is True:
-        checks_passed = False
+        failed()
         print("\n{err}Error{white}[{grey}options_conflict{white}]{red}>{default} you can't use {cyan}--trash{default} toghether with {cyan}--shred{default}\n"\
             .format(**craplog.text_colors))
     if  craplog.less_output is True\
     and craplog.more_output is True:
-        checks_passed = False
+        failed()
         print("\n{err}Error{white}[{grey}options_conflict{white}]{red}>{default} you can't use {cyan}--less{default} toghether with {cyan}--more{default}\n"\
             .format(**craplog.text_colors))
 
@@ -478,36 +487,36 @@ def makeInitialChecks( craplog ):
     if craplog.logs_path.endswith("/"):
         craplog.logs_path = craplog.logs_path[:-1]
     if craplog.logs_path == "":
-        checks_passed = False
+        failed()
         print("\n{err}Error{white}[{grey}missing_argument{white}]{red}>{default} you must set a path after {cyan}-P{default}/{cyan}--logs_path{default}\n"\
             .format(**craplog.text_colors)\
             %(craplog.logs_path))
     else:
         craplog.logs_path = os.path.abspath( craplog.logs_path )
     if checkFolder( craplog, "logs_folder", craplog.logs_path, r=True ) is False:
-        checks_passed = False
+        failed()
 
     # log files
     if craplog.max_file_size is None:
-        checks_passed = False
+        failed()
         print("\n{err}Error{white}[{grey}missing_arguments{white}]{red}>{default} you must set the size after {cyan}--max-size{default}\n"\
             .format(**craplog.text_colors))
     else:
         try:
             craplog.max_file_size = float(craplog.max_file_size)
             if craplog.max_file_size < 0:
-                checks_passed = False
+                failed()
                 print("\n{err}Error{white}[{grey}invalid_size{white}]{red}>{default} the max size must be greater or equal to 0\n"\
                     .format(**craplog.text_colors))
             elif craplog.max_file_size > 10000:
-                checks_passed = False
+                failed()
                 print("\n{err}Error{white}[{grey}invalid_size{white}]{red}>{default} the given max size is huge: {yellow}%.2f GB{default}\n"\
                     .format(**craplog.text_colors)
                     %( craplog.max_file_size / 1024 ))
                 if craplog.less_output is False:
                     print("                     use 0 to have it unlimited")
         except:
-            checks_passed = False
+            failed()
             print("\n{err}Error{white}[{grey}invalid_argument{white}]{red}>{default} invalid value for {cyan}--max-size{default}: {rose}%s{default}\n"\
                 .format(**craplog.text_colors)
                 %( craplog.max_file_size ))
@@ -517,12 +526,12 @@ def makeInitialChecks( craplog ):
         if craplog.error_logs is True:
             craplog.log_files.append("error.log.1")
     if len(craplog.log_files) == 0:
-        checks_passed = False
+        failed()
         print("\n{err}Error{white}[{grey}missing_arguments{white}]{red}>{default} you must set at least one file name after {cyan}--log-files{default}\n"\
             .format(**craplog.text_colors))
     for file_name in craplog.log_files:
         if file_name.find(".log.") < 0:
-            checks_passed = False
+            failed()
             print("\n{err}Error{white}[{grey}invalid_name{white}]{red}>{default} you have inserted an invalid file name: {rose}%s{default}"\
                 .format(**craplog.text_colors)\
                 %( file_name ))
@@ -531,14 +540,14 @@ def makeInitialChecks( craplog ):
                 print("                     please refer to the README.md for more informations")
             break
         elif craplog.log_files.count( file_name ) > 1:
-                checks_passed = False
+                failed()
                 print("\n{err}Error{white}[{grey}duplicate_argument{white}]{red}>{default} you have inserted the same file name twice: {rose}%s{default}\n"\
                     .format(**craplog.text_colors)\
                     %( file_name ))
                 break
         file_path = "%s/%s" %( craplog.logs_path, file_name )
         if checkFile( craplog, "log_file", file_path, r=True ) is False:
-            checks_passed = False
+            failed()
 
     # trash folder
     if craplog.trash is True:
@@ -546,48 +555,48 @@ def makeInitialChecks( craplog ):
             craplog.trash_path = "%s/%s" %( os.environ['HOME'], craplog.trash_path[2:] )
         craplog.trash_path = os.path.abspath( craplog.trash_path )
         if checkFolder( craplog, "trash", craplog.trash_path, w=True ) is False:
-            checks_passed = False
+            failed()
 
     # crapstats main folder
     if checks_passed is True:
         if checkFolder( craplog, "stats_folder", craplog.statpath, r=True, w=True, create=True ) is False:
-            checks_passed = False
+            failed()
     # global stats folder
     if  checks_passed is True\
     and craplog.global_stats is True:
         path = "%s/globals" %( craplog.statpath )
         if checkFolder( craplog, "stats_folder", path, r=True, w=True, create=True ) is False:
-            checks_passed = False
+            failed()
     # sessions stats folder
     if  checks_passed is True\
     and craplog.session_stats is True:
         path = "%s/sessions" %( craplog.statpath )
         if checkFolder( craplog, "stats_folder", path, r=True, w=True, create=True ) is False:
-            checks_passed = False
+            failed()
     # access stats folder
     if  checks_passed is True\
     and craplog.access_logs is True:
         path = "%s/sessions/access" %( craplog.statpath )
         if checkFolder( craplog, "stats_folder", path, r=True, w=True, create=True ) is False:
-            checks_passed = False
+            failed()
     # error stats folder
     if  checks_passed is True\
     and craplog.error_logs is True:
         path = "%s/sessions/error" %( craplog.statpath )
         if checkFolder( craplog, "stats_folder", path, r=True, w=True, create=True ) is False:
-            checks_passed = False
+            failed()
 
     # access logs fields
     if craplog.access_logs is True:
         for field in craplog.access_fields:
             if craplog.access_fields.count( field ) > 1:
-                    checks_passed = False
+                    failed()
                     print("\n{err}Error{white}[{grey}duplicate_argument{white}]{red}>{default} you have inserted the same field twice: {rose}%s{default}\n"\
                         .format(**craplog.text_colors)\
                         %( field ))
                     break
             elif field not in ["IP","UA","REQ","RES"]:
-                checks_passed = False
+                failed()
                 print("\n{err}Error{white}[{grey}invalid_field{white}]{red}>{default} invalid field for access logs: {rose}%s{default}"\
                     .format(**craplog.text_colors)\
                     %( field ))
@@ -605,7 +614,7 @@ def makeInitialChecks( craplog ):
     for ip in craplog.ip_whitelist:
         fc = ip[0]
         if craplog.ip_whitelist.count( ip ) > 1:
-                checks_passed = False
+                failed()
                 print("\n{err}Error{white}[{grey}duplicate_argument{white}]{red}>{default} you have inserted the same IP twice: {rose}%s{default}\n"\
                     .format(**craplog.text_colors)\
                     %( ip ))
@@ -620,7 +629,7 @@ def makeInitialChecks( craplog ):
                 if not( fc == ":"\
                      or fc.isdigit() is True):
                     # useless to keep, it will just slow down the process
-                    checks_passed = False
+                    failed()
                     print("\n{err}Error{white}[{grey}invalid_sequence{white}]{red}>{default} you have inserted an invalid IP sequence: {rose}%s{default}"\
                         .format(**craplog.text_colors)\
                         %( ip ))
@@ -672,12 +681,15 @@ def checkSessionsDates( craplog ):
                 if craplog.less_output is False:
                     print("\nIf you choose to proceed, statistics will be {bold}mergerd{default}"\
                         .format(**craplog.text_colors))
-                    print("Please make sure you're not parsing the same files twice")
+                    if craplog.more_output is True:
+                        print("Please make sure you're not parsing the same files twice\n")
                 proceed = input("Continue? {white}[{grass}y{grey}/{red}n{white}] :{default} "\
                     .format(**craplog.text_colors)).strip().lower()
                 if proceed in ["y","yes"]:
                     break
                 elif proceed in ["n","no"]:
+                    if len(craplog.undo_paths) > 0:
+                        craplog.undoChanges()
                     craplog.exitAborted()
                 else:
                     print("\n{warn}Warning{white}[{grey}choice{white}]{warn}>{default} not a valid choice: {bold}%s{default}"\
