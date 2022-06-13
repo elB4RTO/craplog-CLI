c4dda66e16e2518fd5eb1a0baf2f9524937c1c76
elB4RTO <81904638+elB4RTO@users.noreply.github.com>
2022-06-13 23:19:17 +0200

    Update interface.py

--- crapview/crappy/interface.py
+++ crapview/crappy/interface.py
@@ -24,6 +24,9 @@ class TUI():
         self.terminal = UIobj(
             self.rows, self.cols,
             0, 0 )
+        self.terminal.window = curses.newwin(
+            self.terminal.h, self.terminal.w,
+            self.terminal.y, self.terminal.x )
         # command line
         self.cli = CommandLine(
             4, self.terminal.w-2, # height, width
@@ -31,16 +34,21 @@ class TUI():
         self.cli.window = curses.newwin(
             self.cli.h, self.cli.w, # height, width
             self.cli.y, self.cli.x ) # y_start, x_start
+        self.cli.ui = self
         self.cli.initContent()
         # navigation menu
+        tree_width = int(((self.terminal.w-2)/100)*33.3)-2
+        if tree_width > 32:
+            tree_width = 32
         self.tree = Tree(
             self.terminal.h-2, # h
-            int(((self.terminal.w-2)/100)*33.3)-2, # w
+            tree_width, # w
             self.terminal.y, # x
             self.terminal.x+1) # y
         self.tree.window = curses.newwin(
             self.tree.h, self.tree.w,
             self.tree.y, self.tree.x )
+        self.tree.ui = self
         self.tree.initContent( crappath )
         # stats view
         self.view = View(
@@ -51,19 +59,20 @@ class TUI():
         self.view.window = curses.newwin(
             self.view.h, self.view.w,
             self.view.y, self.view.x )
+        self.view.ui = self
         self.view.initContent()
         # the element with focus
-        self.focusel = 0
+        self.focusel = 1
         self.switchFocus( +1 )
         
     
     
-    def resize(self, new_rows, new_cols ):
+    def resize(self, new_cols, new_rows ):
         """
         Update values about window size
         """
-        self.rows = new_rows
         self.cols = new_cols
+        self.rows = new_rows
         self.adapt()
         self.redraw()
     
@@ -73,21 +82,24 @@ class TUI():
         Adapt the elements to fit the available space
         """
         self.terminal.config(
-            self.rows, self.cols,
-            0, 0 )
+            self.rows, self.cols, # height, width
+            0, 0 ) # y start, x start
         # command line
         self.cli.config(
             4, self.terminal.w-2,
-            self.terminal.endY()-3, self.terminal.x+1 )
+            self.terminal.endY()-2, self.terminal.x+1 )
         # navigation menu
+        tree_width = int(((self.terminal.w-2)/100)*33.3)-2
+        if tree_width > 32:
+            tree_width = 32
         self.tree.config(
-            self.terminal.h-2,
-            int(((self.terminal.w-2)/100)*33.3)-2,
+            self.terminal.h-3,
+            tree_width,
             self.terminal.y,
             self.terminal.x+1)
         # stats view
         self.view.config(
-            self.terminal.h-2,
+            self.terminal.h-3,
             self.terminal.w-self.tree.w-3,
             self.terminal.y,
             self.terminal.x+self.tree.w+2)
@@ -128,20 +140,59 @@ class TUI():
         elif self.focusel > 3:
             self.focusel = 1
         # remove all focuses
-        self.cli.focus = False
+        self.cli.focus  = False
         self.tree.focus = False
         self.view.focus = False
         # switch element
         if self.focusel == 1:
+            curses.curs_set(1)
             self.cli.focus = True
         elif self.focusel == 2:
+            curses.curs_set(0)
             self.tree.focus = True
         elif self.focusel == 3:
+            curses.curs_set(0)
             self.view.focus = True
         else:
             # put an error message here
             raise Exception("\033[1;31m!-> PUT AN ERROR MESSAGE HERE !!!\033[0m")
         # redraw every window
+        self.redraw()
+    
+    
+    def switch2cli(self):
+        """
+        Transfer focus to the cli window
+        """
+        self.focusel = 1
+        curses.curs_set(1)
+        self.cli.focus  = True
+        self.tree.focus = False
+        self.view.focus = False
+        self.redraw()
+    
+    
+    def switch2tree(self):
+        """
+        Transfer focus to the cli window
+        """
+        self.focusel = 2
+        curses.curs_set(0)
+        self.cli.focus  = False
+        self.tree.focus = True
+        self.view.focus = False
+        self.redraw()
+    
+    
+    def switch2view(self):
+        """
+        Transfer focus to the cli window
+        """
+        self.focusel = 3
+        curses.curs_set(0)
+        self.cli.focus  = False
+        self.tree.focus = False
+        self.view.focus = True
         self.redraw()
             
     
@@ -151,7 +202,9 @@ class TUI():
         Redraw the entire screen
         """
         # redraw every window without updating the screen
-        self.tree.redraw(  )
+        self.screen.clear()
+        self.screen.refresh()
+        self.tree.redraw()
         self.view.redraw()
         self.cli.redraw()
     
@@ -166,3 +219,19 @@ class TUI():
             self.view.redraw()
         if self.cli.focus is True:
             self.cli.redraw()
+    
+    
+    def clearStats(self):
+        """
+        Signal to the view window to clear the content
+        """
+        self.view.clearAll()
+    
+    
+    def showStats(self, file_path:str ):
+        """
+        Signal to the view window to show these stats
+        """
+        self.view.inputStats( file_path )
+        self.switch2view()
+
