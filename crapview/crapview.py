774e46dee0449b94f99181b62ac17766ebafba70
elB4RTO <81904638+elB4RTO@users.noreply.github.com>
2022-06-13 23:18:16 +0200

    Update crapview.py

--- crapview/crapview.py
+++ crapview/crapview.py
@@ -18,21 +18,23 @@ def initscr( screen ):
         # system defaults
         curses.init_pair(0, curses.COLOR_WHITE, curses.COLOR_BLACK )
         # text
-        curses.init_pair( 1, curses.COLOR_BLACK,   curses.COLOR_BLACK )
+        curses.init_pair( 1, curses.COLOR_RED,     curses.COLOR_BLACK )
         curses.init_pair( 2, curses.COLOR_GREEN,   curses.COLOR_BLACK )
         curses.init_pair( 3, curses.COLOR_YELLOW,  curses.COLOR_BLACK )
         curses.init_pair( 4, curses.COLOR_BLUE,    curses.COLOR_BLACK )
         curses.init_pair( 5, curses.COLOR_MAGENTA, curses.COLOR_BLACK )
         curses.init_pair( 6, curses.COLOR_CYAN,    curses.COLOR_BLACK )
         curses.init_pair( 7, curses.COLOR_WHITE,   curses.COLOR_BLACK )
+        curses.init_pair( 8, curses.COLOR_BLACK,   curses.COLOR_BLACK )
         # underlined text
-        curses.init_pair( 11, curses.COLOR_BLACK,   curses.COLOR_WHITE )
+        curses.init_pair( 11, curses.COLOR_RED,     curses.COLOR_WHITE )
         curses.init_pair( 12, curses.COLOR_GREEN,   curses.COLOR_WHITE )
         curses.init_pair( 13, curses.COLOR_YELLOW,  curses.COLOR_WHITE )
         curses.init_pair( 14, curses.COLOR_BLUE,    curses.COLOR_WHITE )
         curses.init_pair( 15, curses.COLOR_MAGENTA, curses.COLOR_WHITE )
         curses.init_pair( 16, curses.COLOR_CYAN,    curses.COLOR_WHITE )
         curses.init_pair( 17, curses.COLOR_WHITE,   curses.COLOR_WHITE )
+        curses.init_pair( 18, curses.COLOR_BLACK,   curses.COLOR_WHITE )
     # activate keypad mode
     #screen.keypad( True )
 
