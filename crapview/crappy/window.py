15b58b75afd6d9671a4e40a22f8ee76a19970b73
elB4RTO <81904638+elB4RTO@users.noreply.github.com>
2022-06-13 23:18:54 +0200

    Update window.py

--- crapview/crappy/window.py
+++ crapview/crappy/window.py
@@ -1,6 +1,8 @@
 
 import curses
 
+from os import get_terminal_size
+
 from crappy.interface import TUI
 
 class Window():
@@ -15,16 +17,22 @@ class Window():
         self.n_rows = curses.LINES
         # 
         self.ui = TUI( stdscr, crappath )
+        #
+        self.dry_run = False
     
     
     def checkResized(self):
         """
         If the window has been resized, updates new values
         """
-        if curses.is_term_resized( self.n_rows, self.n_cols ) is True:
-            self.n_cols = curses.COLS
-            self.n_rows = curses.LINES
-            self.ui.resize( self.n_rows, self.n_cols )
+        n_cols, n_rows = get_terminal_size()
+        if n_cols != self.n_cols\
+        or n_rows != self.n_rows:
+            self.n_cols, self.n_rows = n_cols, n_rows
+            curses.resize_term( n_rows, n_cols )
+            self.ui.resize( n_cols, n_rows )
+            self.ui.redraw()
+            self.dry_run = True
     
     
     def run(self):
@@ -38,8 +46,11 @@ class Window():
         while loop is True:
             # fit the available space
             self.checkResized()
-            # wait for an input
-            self.ui.feed( self.screen.getch() )
+            if self.dry_run is True:
+                self.dry_run = False
+            else:
+                # wait for an input
+                self.ui.feed( self.screen.getch() )
             # update the screen
             curses.doupdate()
             #self.screen.refresh() # <-- DELETE IF USELESS
