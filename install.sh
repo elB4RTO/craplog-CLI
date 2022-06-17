ebf709c7bc7c5441d10b795140bd7c60f34e5af0
elB4RTO <elbarto@disroot.org>
2022-06-17 22:29:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ if [ -e /usr/bin/craplog ]
 then
 	while :
 	do
-		printf "\n$(tput bold)$(tput setaf 3)Warning$(tput setaf 15)[$(tput setaf 8)bin$(tput setaf 15)]$(tput setaf 15)>$(tput sgr0) file $(tput setaf 2)/usr/bin/$(tput setaf 9)craplog$(tput sgr0) already exists\n"
+		printf "\n$(tput bold)$(tput setaf 3)Warning$(tput setaf 15)[$(tput setaf 8)bin$(tput setaf 15)]$(tput setaf 3)>$(tput sgr0) file $(tput setaf 2)/usr/bin/$(tput setaf 9)craplog$(tput sgr0) already exists\n"
 		printf "\nIf you choose to continue, the actual file will be lost forever\n"
 		printf "Overwrite file? $(tput setaf 15)[$(tput setaf 2)y$(tput setaf 8)/$(tput setaf 1)n$(tput setaf 15)] :$(tput sgr0) "
 		read agree
@@ -37,154 +37,10 @@ then
 			;;
 		esac
 	done
-fi
-# checking the existence of another Crapview executable
-if [ -e /usr/bin/crapview ]
-then
-	while :
-	do
-		printf "\n$(tput bold)$(tput setaf 3)Warning$(tput setaf 15)[$(tput setaf 8)bin$(tput setaf 15)]$(tput setaf 15)>$(tput sgr0) file $(tput setaf 2)/usr/bin/$(tput setaf 9)crapview$(tput sgr0) already exists\n"
-		printf "\nIf you choose to continue, the actual file will be lost forever\n"
-		printf "Overwrite file? $(tput setaf 15)[$(tput setaf 2)y$(tput setaf 8)/$(tput setaf 1)n$(tput setaf 15)] :$(tput sgr0) "
-		read agree
-		case "$agree"
-		in
-			"y" | "Y" | "yes" | "Yes" | "YES" )
-				printf "\n"
-				break
-			;;
-			"n" | "N" | "no" | "No" | "NO" )
-				printf "\n$(tput bold)$(tput setaf 1)Installation ABORTED$(tput sgr0)\n\n"
-				exit
-			;;
-			*)
-				printf "\n$(tput bold)$(tput setaf 3)Warning$(tput setaf 15)[$(tput setaf 8)choice$(tput setaf 15)]$(tput setaf 15)>$(tput sgr0) not a valid choice: $(tput bold)%s$(tput sgr0)\n"
-				sleep 1
-			;;
-		esac
-	done
-fi
-# checking the existence of another Crapup executable
-if [ -e /usr/bin/crapup ]
-then
-	while :
-	do
-		printf "\n$(tput bold)$(tput setaf 3)Warning$(tput setaf 15)[$(tput setaf 8)bin$(tput setaf 15)]$(tput setaf 15)>$(tput sgr0) file $(tput setaf 2)/usr/bin/$(tput setaf 9)crapup$(tput sgr0) already exists\n"
-		printf "\nIf you choose to continue, the actual file will be lost forever\n"
-		printf "Overwrite file? $(tput setaf 15)[$(tput setaf 2)y$(tput setaf 8)/$(tput setaf 1)n$(tput setaf 15)] :$(tput sgr0) "
-		read agree
-		case "$agree"
-		in
-			"y" | "Y" | "yes" | "Yes" | "YES" )
-				printf "\n"
-				break
-			;;
-			"n" | "N" | "no" | "No" | "NO" )
-				printf "\n$(tput bold)$(tput setaf 1)Installation ABORTED$(tput sgr0)\n\n"
-				exit
-			;;
-			*)
-				printf "\n$(tput bold)$(tput setaf 3)Warning$(tput setaf 15)[$(tput setaf 8)choice$(tput setaf 15)]$(tput setaf 15)>$(tput sgr0) not a valid choice: $(tput bold)%s$(tput sgr0)\n"
-				sleep 1
-			;;
-		esac
-	done
-fi
-# checking the existence of another Crapset executable
-if [ -e /usr/bin/crapset ]
-then
-	while :
-	do
-		printf "\n$(tput bold)$(tput setaf 3)Warning$(tput setaf 15)[$(tput setaf 8)bin$(tput setaf 15)]$(tput setaf 15)>$(tput sgr0) file $(tput setaf 2)/usr/bin/$(tput setaf 9)crapset$(tput sgr0) already exists\n"
-		printf "\nIf you choose to continue, the actual file will be lost forever\n"
-		printf "Overwrite file? $(tput setaf 15)[$(tput setaf 2)y$(tput setaf 8)/$(tput setaf 1)n$(tput setaf 15)] :$(tput sgr0) "
-		read agree
-		case "$agree"
-		in
-			"y" | "Y" | "yes" | "Yes" | "YES" )
-				printf "\n"
-				break
-			;;
-			"n" | "N" | "no" | "No" | "NO" )
-				printf "\n$(tput bold)$(tput setaf 1)Installation ABORTED$(tput sgr0)\n\n"
-				exit
-			;;
-			*)
-				printf "\n$(tput bold)$(tput setaf 3)Warning$(tput setaf 15)[$(tput setaf 8)choice$(tput setaf 15)]$(tput setaf 15)>$(tput sgr0) not a valid choice: $(tput bold)%s$(tput sgr0)\n"
-				sleep 1
-			;;
-		esac
-	done
-fi
-
-# checking the existence of another Craplog folder in the home
-if [ -e "$home/.craplog" ]
-then
-	if [ -d "$home/.craplog" ]
-	then
-		while :
-		do
-			printf "\n$(tput bold)$(tput setaf 3)Warning$(tput setaf 15)[$(tput setaf 8)directory$(tput setaf 15)]$(tput setaf 15)>$(tput sgr0) directory $(tput setaf 2)$home/$(tput setaf 9).craplog$(tput sgr0) already exists\n"
-			printf "\nException made for the $(tput bold)crapstats$(tput sgr0), yf you choose to continue the actual content will be lost forever\n"
-			printf "Overwrite content? $(tput setaf 15)[$(tput setaf 2)y$(tput setaf 8)/$(tput setaf 1)n$(tput setaf 15)] :$(tput sgr0) "
-			read agree
-			case "$agree"
-			in
-				"y" | "Y" | "yes" | "Yes" | "YES" )
-					printf "\n"
-					cd "$home/.craplog/"
-					for entry in *
-					do
-						if [[ "$entry" != "crapstats" ]]
-						then
-							if [ -d "$entry" ]
-							then
-								rm -r "$entry" &> /dev/null && wait
-							else
-								rm "$entry" &> /dev/null && wait
-							fi
-						fi
-					done
-					break
-				;;
-				"n" | "N" | "no" | "No" | "NO" )
-					printf "\n$(tput bold)$(tput setaf 1)Installation ABORTED$(tput sgr0)\n\n"
-					exit
-				;;
-				*)
-					printf "\n$(tput bold)$(tput setaf 3)Warning$(tput setaf 15)[$(tput setaf 8)choice$(tput setaf 15)]$(tput setaf 15)>$(tput sgr0) not a valid choice: $(tput bold)%s$(tput sgr0)\n"
-					sleep 1
-				;;
-			esac
-		done
-	else
-		while :
-		do
-			printf "\n$(tput bold)$(tput setaf 3)Warning$(tput setaf 15)[$(tput setaf 8)conflict$(tput setaf 15)]$(tput setaf 15)>$(tput sgr0) an entry for $(tput setaf 2)$home/$(tput setaf 9).craplog$(tput sgr0) already exists\n"
-			printf "\nIf you choose to continue, the actual entry will be lost forever\n"
-			printf "Delete entry? $(tput setaf 15)[$(tput setaf 2)y$(tput setaf 8)/$(tput setaf 1)n$(tput setaf 15)] :$(tput sgr0) "
-			read agree
-			case "$agree"
-			in
-				"y" | "Y" | "yes" | "Yes" | "YES" )
-					printf "\n"
-					rm "$home/.craplog" &> /dev/null && wait
-					break
-				;;
-				"n" | "N" | "no" | "No" | "NO" )
-					printf "\n$(tput bold)$(tput setaf 1)Installation ABORTED$(tput sgr0)\n\n"
-					exit
-				;;
-				*)
-					printf "\n$(tput bold)$(tput setaf 3)Warning$(tput setaf 15)[$(tput setaf 8)choice$(tput setaf 15)]$(tput setaf 15)>$(tput sgr0) not a valid choice: $(tput bold)%s$(tput sgr0)\n"
-					sleep 1
-				;;
-			esac
-		done
-	fi
 fi
 
 
 # start installing
-printf "Installing Craplog $(tput setaf 15)...$(tput sgr0)"
+printf "Installing Craplog $(tput setaf 15)...$(tput sgr0) "
+
 
