0accf1184dc92830ae9fddd76b05d0ab9d5e1687
elB4RTO <elbarto@disroot.org>
2022-06-17 22:20:56 +0200

    Update aux.py

--- crapup/crappy/aux.py
+++ crapup/crappy/aux.py
@@ -1,47 +1,15 @@
 
-def elbarto() -> str :
-    return """\
-░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-░░░░░░░░░░░░░██░░░░░░░░░░░░░░░░░░██████████░░░░░░░░░░░░░░░░░░░░
-░░░░░███████░██░░░░░░░░░░░░░░░░░░░░░░█░░░░░░░░█░█░█░█░█░█░░░░░░
-░░░░░░█░░░░░░██░░░░░███░░░░██░░████░░█░████░░░███████████░░░░░░
-░░░░░░████░░░██░░░░░█░░█░░█░█░░░█░░█░█░█░░█░░░███████████░░░░░░
-░░░░░░█░░░░░░██░░░░░███░░░█░░█░░███░░█░█░░█░░░███████████░ ░░░░
-░░░░░░██████░█░░░░░░█░░█░██████░█░░█░░░████░░░            ░░░░░
-░░░░░░░░░░░░░░░░░░░░███░░█░░░░█░█░░█░░░░░░░░░░  ▄       ▄░ ░░░░
-░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░███████████░░░░░░
-░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░███████████░░░░░░
-░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░████░░░░░░
-░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\
-"""
-
-
-def craplogo() -> str :
-    return """\
-     __    ___     _____    ___     _    _    _____ 
-    / _|  |   \\   |  _  |  |   \\   | |  | |  |  _  |
-   | /    | || |  | |_| |  | || |  | |  | |  | | | |
-   | |    |   /   |  _  |  |  _/   | |  | |  |  ___|
-   | \\_   |   \\   | | | |  | |     | |__| |  | |    
-    \\__|  |_|\\_\\  |_| |_|  |_|     |______|  |_|    \
-"""
-
-
-def help( color_set ) -> str :
+def MSG_help( color_set:dict ) -> str :
     return """\
 {err}Synopsis{default}
 
-    crapup {grey}[{white}OPTION{grey}]{default}
+    {grey}craplog{default} update {grey}[{white}OPTION{grey}]{default}
 
 
 {err}Options{default}
 
                  {yellow}Option{default}  ¦  {yellow}Description{default}
-{white}-------------------------------------------------------------------------------{default}
+{white}--------------------------------------------------------------------------------{default}
                          ¦
                      {bold}-h{default}  ¦  print this screen and exit
                  {bold}--help{default}  ¦
@@ -63,90 +31,22 @@ def help( color_set ) -> str :
                          ¦
                   {bold}--git{default}  ¦  update Craplog with a git-pull
                          ¦
-{white}-------------------------------------------------------------------------------{default}\
+{white}--------------------------------------------------------------------------------{default}\
 """.format(**color_set).replace("¦", "{white}¦{default}".format(**color_set))
 
 
-def examples( color_set ) -> str :
+def MSG_examples( color_set:dict ) -> str :
     return """\
 {err}Examples{default}
 
    - {green}Check for a new version and print the response message.{default}
      
-       {italic}crapup{default}
+       {italic}craplog{default} {azul}update{default}
 
 
    - {green}Fetch every new change from the remote git repository and apply it.
      This is made using system's {bold}{cyan}git{default}{cyan} package.
      If Craplog's local git has not been initialized yet, offers to do so.{default}
      
-       {italic}craplog{default} {bold}--git{default}\
+       {italic}craplog{default} {azul}update{default} {bold}--git{default}\
 """.format(**color_set)
-
-
-def crapup( color_set ) -> str :
-    return """{bold}\
-   {red} CCCC{default}{bold}  {orange}RRRR {default}{bold}  {grass}AAAAA{default}{bold}  {cyan}PPPP {default}{bold}  {white}U   U{default}{bold}  {white}PPPPP{default}{bold}
-   {red}C    {default}{bold}  {orange}R   R{default}{bold}  {grass}A   A{default}{bold}  {cyan}P   P{default}{bold}  {white}U   U{default}{bold}  {white}P   P{default}{bold}
-   {red}C    {default}{bold}  {orange}RRRR {default}{bold}  {grass}AAAAA{default}{bold}  {cyan}PPPP {default}{bold}  {white}U   U{default}{bold}  {white}PPPPP{default}{bold}
-   {red}C    {default}{bold}  {orange}R  R {default}{bold}  {grass}A   A{default}{bold}  {cyan}P    {default}{bold}  {white}U   U{default}{bold}  {white}P    {default}{bold}
-   {red} CCCC{default}{bold}  {orange}R   R{default}{bold}  {grass}A   A{default}{bold}  {cyan}P    {default}{bold}  {white}UUUUU{default}{bold}  {white}P    {default}\
-""".format(**color_set)
-
-
-def fin( color_set ) -> str :
-    return """{bold}\
-   {orange}FFFFF{default}{bold}  {grass}II{default}{bold}  {cyan}N   N{default}{bold}
-   {orange}F    {default}{bold}  {grass}II{default}{bold}  {cyan}NN  N{default}{bold}
-   {orange}FFF  {default}{bold}  {grass}II{default}{bold}  {cyan}N N N{default}{bold}
-   {orange}F    {default}{bold}  {grass}II{default}{bold}  {cyan}N  NN{default}{bold}
-   {orange}F    {default}{bold}  {grass}II{default}{bold}  {cyan}N   N{default}\
-""".format(**color_set)
-
-
-def colors() -> dict :
-    return { 'default' : "\033[0m",
-             'bold'    : "\033[1m",
-             'italic'  : "\033[3m",
-             'black'   : "\033[30m",
-             'grey'    : "\033[90m",
-             'white'   : "\033[37m",
-             'paradise': "\033[97m",
-             'purple'  : "\033[35m",
-             'pink'    : "\033[95m",
-             'blue'    : "\033[34m",
-             'azul'    : "\033[94m",
-             'cyan'    : "\033[36m",
-             'grass'   : "\033[32m",
-             'green'   : "\033[92m",
-             'red'     : "\033[31m",
-             'rose'    : "\033[91m",
-             'orange'  : "\033[33m",
-             'yellow'  : "\033[93m",
-             'ok'      : "\033[1;32m",
-             'err'     : "\033[1;31m",
-             'warn'    : "\033[1;33m" }
-
-
-def no_colors() -> dict :
-    return { 'default' : "",
-             'bold'    : "",
-             'italic'  : "",
-             'black'   : "",
-             'grey'    : "",
-             'white'   : "",
-             'paradise': "",
-             'purple'  : "",
-             'pink'    : "",
-             'blue'    : "",
-             'azul'    : "",
-             'cyan'    : "",
-             'grass'   : "",
-             'green'   : "",
-             'red'     : "",
-             'rose'    : "",
-             'orange'  : "",
-             'yellow'  : "",
-             'ok'      : "",
-             'err'     : "",
-             'warn'    : "" }
