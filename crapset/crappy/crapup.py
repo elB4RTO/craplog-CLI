9ec1ab3c001cfb37a458cccea926b1645e00ddfd
elB4RTO <elbarto@disroot.org>
2022-06-19 23:52:06 +0200

    Update crapup.py

--- crapset/crappy/crapup.py
+++ crapset/crappy/crapup.py
@@ -18,7 +18,7 @@ class UpSet(ModelSet):
             'configs'   : True,
             'arguments' : True,
             'less output' : False,
-            'more_output' : False,
+            'more output' : False,
             'colors'      : True,
             'git' : False
         }
@@ -138,9 +138,9 @@ class UpSet(ModelSet):
                 with open(self.file_path,'w') as f:
                     f.write( configs )
                     if supercrap.more_output is True:
-                        print("\n{ok}Succesfully written configurations for {bold}CRAP{white}UP{default}\n"\
+                        print("\n{ok}Succesfully written configurations for {bold}%s{default}\n"\
                             .format(**supercrap.text_colors)\
-                            %( self.file_path ))
+                            %( supercrap.TXT_crapup ))
             except:
                 # failed to write
                 result = False
@@ -161,8 +161,8 @@ class UpSet(ModelSet):
         """ Check the integrity of the configuration """
         
         checks_passed = True
-        if  self.sets_map['less_output'] is True\
-        and self.sets_map['more_output'] is True:
+        if  self.sets_map['less output'] is True\
+        and self.sets_map['more output'] is True:
             checks_passed = False
             if supercrap.less_output is False:
                 print()
